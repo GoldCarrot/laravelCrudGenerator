081bffcc356da288ca927d763fe5ffc79a40cfff
chatway <denzlbro@gmail.com>
2021-11-29 10:42:00 +0500

    update

--- src/Core/Templates/Views/Form/selectEnum.blade.php
+++ src/Core/Templates/Views/Form/selectEnum.blade.php
@@ -6,8 +6,8 @@
 /* @var $viewGenerator \Chatway\LaravelCrudGenerator\Core\Generators\ViewGenerator */
 /* @var $propertyDTO \Chatway\LaravelCrudGenerator\Core\DTO\PropertyDTO */
 ?>
-<?= "{{ BsForm::select('$propertyDTO->name')
-                                ->value(old('$propertyDTO->name', \\{$propertyDTO->enum->enumName}::labels()))
+<?= "{{ BsForm::select('$propertyDTO->name', \\{$propertyDTO->enum->enumName}::labels())
+                                ->value(old('$propertyDTO->name'))
                                 ->placeholder(__('admin.columns.{$propertyDTO->name}'))
                                 ->label(__('admin.columns.{$propertyDTO->name}'))
                         }}" . PHP_EOL ?>
