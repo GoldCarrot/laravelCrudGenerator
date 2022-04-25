7826161acfbefde80384ff568949fe8a0af3c656
Yunusbaev Denis <dy@goldcarrot.ru>
2022-04-25 17:43:26 +0500

    fix model.blade.php

--- src/Core/Templates/Classes/model.blade.php
+++ src/Core/Templates/Classes/model.blade.php
@@ -15,7 +15,7 @@ namespace {{ $generator->generatorForm->getNsByClassName($generator->generatorFo
 
 use {{ $generator->baseClass }};
 use Eloquent;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 <?php /** Начало прикрепления классов для внутренних ключей **/ ?>
 <?php $addedClasses = [] ?>
 @foreach($generator->generatorForm->internalForeignKeys as $internalForeignKey)
